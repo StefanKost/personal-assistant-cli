3b134b2a45ecdb7bab351612ddef25483cccb51e
Vasyl Domanchuk <domanchukits@gmail.com>
2025-11-11 20:39:44 +0100

    add Note tags

--- models/note.py
+++ models/note.py
@@ -1,17 +1,67 @@
-from models.values import Field, Tag
+from typing import Self
+from datetime import datetime as DateTime
+from models.values import Field, Tag, Title
 
 
 class Note:
-
-    def __init__(self, text: str):
-        if not text or not text.strip():
-            raise ValueError("Note cannot be empty")
-
-        self.text = Field(text.strip())
-        self.tags: list[Tag] = []
-
-    # TODO: Implement logic to add tags and update text
+    def __init__(self, title: str, body: str = "", tags: set[Tag] = {}):
+        self.title: Title = Title(title.strip())
+        self.body: Field = Field(body.strip())
+        self.tags: set[Tag] = tags
+        self.created_at: DateTime = DateTime.now()
+        self.updated_at: DateTime = None
 
     def __str__(self) -> str:
-        # TODO: Implement not preview
-        pass
+        return (
+            f"Title: {self.title}\n"
+            f"Body: {self.body}\n"
+            f"Tags: {', '.join(self.tags)}\n"
+            f"Created at: {self.created_at}\n"
+            f"Updated at: {self.updated_at}"
+        )
+
+    def edit_title(self, new_title: str) -> Self:
+        """Edit the title of the note"""
+        self.title = Title(new_title.strip())
+        self.updated_at = DateTime.now()
+
+        return self
+
+    def edit_body(self, new_body: str) -> Self:
+        """Edit the body of the note"""
+        self.body = Title(new_body)
+        self.updated_at = DateTime.now()
+
+        return self
+
+    def add_tags(self, *tags: Tag) -> Self:
+        """Add tags to the note"""
+        updated = False
+
+        for tag in tags:
+            if tag in self.tags:
+                continue  # to track if it's required to updated_at
+
+            updated = True
+            self.tags.add(Tag(tag))
+
+        if updated:
+            self.updated_at = DateTime.now()
+
+        return self
+
+    def remove_tags(self, *tags: Tag) -> Self:
+        """Remove tags from the note"""
+        updated = False
+
+        for tag in tags:
+            if tag not in self.tags:
+                continue  # to track if it's required to updated_at
+
+            updated = True
+            self.tags.remove(tag)
+
+        if updated:
+            self.updated_at = DateTime.now()
+
+        return self
